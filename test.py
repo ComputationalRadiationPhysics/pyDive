b0f9e120fd80dcb15cfe9e12984bef9c78be0fbb
Heikman <h.burau@hzdr.de>
2014-07-14 15:09:00 +0200

    update test.py

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ from h5_ndarray.factories import *
 import algorithm
 import numpy as np
 
-s = fromPath("/home/burau/test.h5", "/data/1000/fields/FieldE/x", 0, np.s_[:,:,:])
-#s = fromPath("/net/cns/projects/HPL/electrons/burau/", "/data/1000/fields/FieldE/x", 0, np.s_[:,:,:])
+#s = fromPath("/home/burau/test.h5", "/data/1000/fields/FieldE/x", 0, np.s_[:,:,:])
+s = fromPath("/net/cns/projects/HPL/electrons/burau/test.h5", "/data/1000/fields/FieldE/x", 0, np.s_[:,:,:])
 
 #s = fromPath("/net/cns/projects/HPL/electrons/huebl/2014-07-02_khi-shortRad/simOutput/h5_data_1400.h5",\
 #    "/data/1400/fields/FieldB", 0, np.s_[0,:,:])
